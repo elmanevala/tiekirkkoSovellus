7e2f5102b1f941970babe7a090f5360005f340b8
Elma Nevala <elma.nevala@helsinki.fi>
2020-05-17 22:18:37 +0300

    requirments kuntoon, vierailujen listaus ja mahdollisuus merkata opas paikalle

--- application/visits/views.py
+++ application/visits/views.py
@@ -1,5 +1,5 @@
 from application import db, app
-from flask import render_template, request
+from flask import redirect, url_for, render_template, request
 from application.visits.models import Visit
 
 @app.route("/visits/new/")
@@ -13,4 +13,17 @@ def visits_create():
     db.session().add(v)
     db.session().commit()
   
-    return "hello world!"
+    return redirect(url_for("visits_index"))
+
+@app.route("/visit", methods=["GET"])
+def visits_index():
+    return render_template("visits/list.html", visits = Visit.query.all())
+
+@app.route("/visits/<visit_id>/", methods=["POST"])
+def tourguide_set_present(visit_id):
+
+    v = Visit.query.get(visit_id)
+    v.tourguide = True
+    db.session().commit()
+  
+    return redirect(url_for("visits_index"))
