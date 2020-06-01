4d3c38cfcc3d846c066daa6a8cdaddff7a955045
Elma Nevala <elma.nevala@helsinki.fi>
2020-06-01 15:59:27 +0300

    Kirkoille omat näkymät

--- application/churches/models.py
+++ application/churches/models.py
@@ -8,13 +8,12 @@ class Church(db.Model):
     church = db.Column(db.String(144), nullable=False)
     town = db.Column(db.String(144), nullable=False)
 
-
     visits = db.relationship("Visit", backref='Church', lazy=True)
 
     def __init__(self, church, town):
         self.church = church
         self.town = town
-  
+
     def get_id(self):
         return self.id
 
@@ -22,29 +21,44 @@ class Church(db.Model):
     def town_churches_visits():
 
         stmt = text("SELECT DISTINCT A.town AS town, "
-        "(SELECT COUNT(*) FROM Church B WHERE B.town=A.town) AS churches, "
-        "(SELECT COUNT(V.comment) FROM Church C LEFT JOIN Visit V ON C.id=V.church_id AND C.town=A.town) AS visits"
-        " FROM Church A")
+                    "(SELECT COUNT(*) FROM Church B WHERE B.town=A.town) AS churches, "
+                    "(SELECT COUNT(V.comment) FROM Church C LEFT JOIN Visit V ON C.id=V.church_id AND C.town=A.town) AS visits"
+                    " FROM Church A")
 
         res = db.engine.execute(stmt)
 
         response = []
         for row in res:
-            response.append({"town":row[0], "churches":row[1], "visits":row[2]})
+            response.append(
+                {"town": row[0], "churches": row[1], "visits": row[2]})
 
         return response
 
     @staticmethod
     def churches_in_town(town):
 
-        stmt = text("SELECT church FROM Church WHERE town=:town").params(town=town)
+        stmt = text("SELECT church FROM Church WHERE town=:town").params(
+            town=town)
 
         res = db.engine.execute(stmt)
 
-        print("TÄÄLLÄLÄLÄLÄLÄLÄLÄ inceptionin lumikerros:)")
+        response = []
+        for row in res:
+            response.append({"church": row[0]})
+
+        return response
+
+    @staticmethod
+    def church_comments(church):
+
+        stmt = text("SELECT V.comment, V.date_created FROM Church C JOIN Visit V ON C.id=V.church_id AND C.church=:church").params(
+            church=church)
+
+        res = db.engine.execute(stmt)
 
         response = []
         for row in res:
-            response.append({"church":row[0]})
-
+            response.append({"comment": row[0]})
+            response.append({"date": row[1]})
+            
         return response
--- application/churches/views.py
+++ application/churches/views.py
@@ -14,4 +14,12 @@ def towns_index():
 @app.route("/churches/<town_name>/", methods=["GET", "POST"])
 @login_required
 def town_churchview(town_name):
-    return render_template("churches/churches.html", comments=Church.churches_in_town(town_name), town=town_name)
+    return render_template("churches/churches.html", churches=Church.churches_in_town(town_name), town=town_name)
+
+@app.route("/churches/<town_name>/<church>/", methods=["GET", "POST"])
+@login_required
+def church_comment(church, town_name):
+
+    kirkko = Church.query.filter(Church.church==church).first()
+    
+    return render_template("churches/church.html", comments=Church.church_comments(church), church=Church.query.filter(Church.church==church).first())
