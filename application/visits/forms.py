1ae2f9034586bdc2f2403170f869f0d3d80952cd
Elma Nevala <elma.nevala@helsinki.fi>
2020-06-16 17:24:40 +0300

    Kommentin maksimipituus SQLalchemyn hyväksymäksi

--- application/visits/forms.py
+++ application/visits/forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField, BooleanField, validators
 
 class VisitForm(FlaskForm):
     church = StringField("Kirkko",  [validators.Length(min=2, max=50)], render_kw={"placeholder": "kirkko"})
-    comment = StringField("Kommentti",  [validators.Length(min=2, max=300, message='Kommentin oltava 3-500 merkkiä')], render_kw={"placeholder": "kommentti"})
+    comment = StringField("Kommentti",  [validators.Length(min=2, max=140, message='Kommentin oltava 3-140 merkkiä')], render_kw={"placeholder": "kommentti"})
     tourguide = BooleanField("Opas paikalla")
     class Meta:
         csrf = False
 
 
 class EditForm(FlaskForm):
-    comment = StringField("Uusi kommentti",  [validators.Length(min=2, max=300, message='Kommentin oltava 3-500 merkkiä')], render_kw={"placeholder": "kommentti"})
+    comment = StringField("Uusi kommentti",  [validators.Length(min=2, max=140, message='Kommentin oltava 3-140 merkkiä')], render_kw={"placeholder": "kommentti"})
 
     class Meta:
         csrf = False
